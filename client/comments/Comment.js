037066dca07eb247a1bfb3a73ed31d9a134ebf2b
Gustavo Torres <gustavotorresds@gmail.com>
2018-05-26 09:30:09 -0300

    Lots of materialize styling. Changed some of the comment/like display

--- client/comments/Comment.js
+++ client/comments/Comment.js
@@ -4,5 +4,9 @@ Template.Comment.helpers({
     commentText: function() {
         var text = Comments.find({_id: this});
         return text;
+    },
+    authorUsername: function() {
+        var user = Meteor.users.findOne({_id: this.author});
+        return user.username;
     }
 });
